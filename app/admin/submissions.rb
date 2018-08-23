2714451243095a82c51796924a166368d4de01f8
Raghu Betina <raghu@firstdraft.com>
2018-08-23 00:57:51 +0000

    Whitelist columns in Submission for ActiveAdmin

--- app/admin/submissions.rb
+++ app/admin/submissions.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Submission do
+
+ permit_params :csv_attachment, :user_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
