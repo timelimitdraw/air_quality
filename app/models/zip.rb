d75ce17549b6b0dcc26378ddfe22a7f23247de89
Raghu Betina <raghu@firstdraft.com>
2018-08-23 00:57:21 +0000

    Validate presence of zips.zip_code

--- app/models/zip.rb
+++ app/models/zip.rb
@@ -5,6 +5,8 @@ class Zip < ApplicationRecord
 
   # Validations
 
+  validates :zip_code, :presence => { :message => "You must input a zip code" }
+
   validates :zip_code, :numericality => { :greater_than => 0 }
 
   validates :zip_code, :length => { :minimum => 5, :maximum => 5 }
